8881792b1a2e218c53d13b6b1a6e013d6a841098
OceanCT <yangzhichao_hit@163.com>
2022-04-06 23:35:29 +0800

    Controller

--- Controller/UserServiceController/userServiceController.go
+++ Controller/UserServiceController/userServiceController.go
@@ -5,6 +5,7 @@ import (
 	"TheLabSystem/Service/UserService"
 	"TheLabSystem/Types/RequestAndResponseType/ChangeUserInfoRequestAndResponse"
 	"TheLabSystem/Types/RequestAndResponseType/ErrNo"
+	"TheLabSystem/Types/RequestAndResponseType/RegisterUserRequestAndResponse"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -32,5 +33,13 @@ func (controller UserServiceController) ChangeUserInfo(c *gin.Context) {
 }
 
 func (controller UserServiceController) RegisterUser(c *gin.Context) {
-
+	request := &RegisterUserRequestAndResponse.RegisterUserRequest{}
+	if err := c.ShouldBindJSON(request); err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+		return
+	}
+	response := &RegisterUserRequestAndResponse.RegisterUserResponse{}
+	response.Code = UserService.UserService{}.RegisterUser(request.Username, request.Password, request.UserType, request.VerifyCode)
+	response.Data.Message = ErrorInformation.GenerateErrorInformation(response.Code)
+	c.JSON(http.StatusOK, response)
 }
