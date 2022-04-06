9a584adb7b45d8fb8c29bcc60a294a1c6aa0611f
OceanCT <yangzhichao_hit@163.com>
2022-04-06 20:11:01 +0800

    router

--- Router/router.go
+++ Router/router.go
@@ -1,12 +1,18 @@
 package Router
 
 import (
-	"TheLabSystem/Controller"
+	"TheLabSystem/Controller/AuthController"
+	"TheLabSystem/Controller/UserServiceController"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRouter(r *gin.Engine) {
 	g := r.Group("/api/v1")
+	// auth
+	g.POST("/auth/login", AuthController.AuthController{}.Login)
+	g.POST("/auth/logout", AuthController.AuthController{}.Logout)
+	g.POST("/auth/whoAmI", AuthController.AuthController{}.WhoAmI)
 
-	g.POST("/auth/login", Controller.AuthController{}.Login)
+	// user
+	g.POST("/user/changeUserInfo", UserServiceController.UserServiceController{}.ChangeUserInfo)
 }
