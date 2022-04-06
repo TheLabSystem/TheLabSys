2cdca1b62fa20aea3dbbd01fc0d2d176f6f3940f
OceanCT <yangzhichao_hit@163.com>
2022-04-06 23:36:04 +0800

    Service

--- Service/UserService/userService.go
+++ Service/UserService/userService.go
@@ -3,6 +3,7 @@ package UserService
 import (
 	"TheLabSystem/Dao/UserDao"
 	"TheLabSystem/Dao/UserInfoDao"
+	"TheLabSystem/Dao/VerifyCodeDao"
 	"TheLabSystem/Types/RequestAndResponseType/ErrNo"
 	"TheLabSystem/Types/ServiceType/User"
 	"fmt"
@@ -42,6 +43,31 @@ func (userService UserService) ChangeUserInfo(username string, password string, 
 		if UserInfoDao.ChangeUserInfo(user.UserID, userInfo) != nil {
 			return ErrNo.UnknownError
 		}
+	}
+	return ErrNo.OK
+}
+func (userService UserService) RegisterUser(username string, password string, userType int, verify_code int) ErrNo.ErrNo {
+	verify, err := VerifyCodeDao.CheckVerifyCode(verify_code, userType)
+	if err != nil {
+		return ErrNo.UnknownError
+	} else if verify == false {
+		return ErrNo.VerifyCodeNotVaild
+	}
+	var user User.User
+	user, err = UserDao.FindUserByUsername(username)
+	if err != nil {
+		return ErrNo.UnknownError
+	}
+	if user.Username != "" {
+		return ErrNo.UserHasExisted
+	}
+	user.Username = username
+	user.DisplayName = username
+	user.UserType = userType
+	user.Money = 0
+	user.Password = password
+	if err = UserDao.InsertUser(user); err != nil {
+		return ErrNo.UnknownError
 	}
 	return ErrNo.OK
 }
