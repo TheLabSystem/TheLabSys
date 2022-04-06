ecca2016a69664920a9a057f95be90896b6ab811
OceanCT <yangzhichao_hit@163.com>
2022-04-06 18:38:11 +0800

    AuthController

---
+++ Controller/AuthController/authController.go
@@ -0,0 +1,70 @@
+package AuthController
+
+import (
+	"TheLabSystem/Dao/UserDao"
+	"TheLabSystem/Service/UserService"
+	"TheLabSystem/Types/RequestAndResponseType/ErrNo"
+	"TheLabSystem/Types/RequestAndResponseType/LoginRequestAndResponse"
+	"TheLabSystem/Types/RequestAndResponseType/LogoutRequestAndResponse"
+	"TheLabSystem/Types/RequestAndResponseType/WhoAmIRequestAndResponse"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type AuthController struct {
+}
+
+func (controller AuthController) Login(c *gin.Context) {
+	loginRequest := &LoginRequestAndResponse.LoginRequest{}
+	if err := c.ShouldBindJSON(&loginRequest); err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+		return
+	}
+	// checkUserPassword
+	var userService UserService.UserService
+	errNo, user, userinfo := userService.CheckPassword(loginRequest.Username, loginRequest.Password)
+	loginResponse := &LoginRequestAndResponse.LoginResponse{}
+	loginResponse.Code = errNo
+	loginResponse.Data.Userinfo = userinfo
+	loginResponse.Data.User = user
+	if errNo == ErrNo.OK {
+		loginResponse.Data.Message = "Log in successfully"
+	} else if errNo == ErrNo.WrongPassword {
+		loginResponse.Data.Message = "Wrong Password!"
+	} else if errNo == ErrNo.UnknownError {
+		loginResponse.Data.Message = "An unexpected error has happened.Please try again."
+	}
+	c.SetCookie("camp-session", loginRequest.Username, 0, "/", "/", false, false)
+	c.JSON(http.StatusOK, loginResponse)
+}
+
+func (controller AuthController) Logout(c *gin.Context) {
+	logoutResponse := &LogoutRequestAndResponse.LogoutResponse{}
+
+	cookie, err := c.Request.Cookie("camp-session")
+	if err == nil {
+		c.SetCookie(cookie.Name, cookie.Value, -1, cookie.Path, cookie.Domain, cookie.Secure, cookie.HttpOnly)
+		logoutResponse.Code = ErrNo.OK
+	} else {
+		logoutResponse.Code = ErrNo.LoginRequired
+	}
+	c.JSON(http.StatusOK, logoutResponse)
+}
+
+func (controller AuthController) WhoAmI(c *gin.Context) {
+	whoAmIResponse := WhoAmIRequestAndResponse.WhoAmIResponse{}
+	cookie, err := c.Request.Cookie("camp-session")
+	if err != nil {
+		whoAmIResponse.Code = ErrNo.LoginRequired
+		return
+	}
+	whoAmIResponse.Data.User, err = UserDao.FindUserByUsername(cookie.Value)
+	if err != nil {
+		whoAmIResponse.Code = ErrNo.UnknownError
+	} else if whoAmIResponse.Data.User.UserID == 0 {
+		whoAmIResponse.Code = ErrNo.UserNotExisted
+	} else {
+		whoAmIResponse.Code = ErrNo.OK
+	}
+	c.JSON(http.StatusOK, whoAmIResponse)
+}
