710ffeb3eaee9f87c02079986d88b8a7a04091cd
OceanCT <yangzhichao_hit@163.com>
2022-04-06 20:09:55 +0800

    errorInformation

---
+++ Config/ErrorInformation/errorInformation.go
@@ -0,0 +1,25 @@
+package ErrorInformation
+
+import "TheLabSystem/Types/RequestAndResponseType/ErrNo"
+
+func GenerateErrorInformation(code ErrNo.ErrNo) string {
+	if code == ErrNo.UnknownError {
+		return "An unexpected error has happened.Please try again."
+	}
+	if code == ErrNo.WrongPassword {
+		return "Wrong Password!"
+	}
+	if code == ErrNo.LoginRequired {
+		return "Your information is out of date.Please log in first."
+	}
+	if code == ErrNo.UserNotExisted {
+		return "There is no such user.Maybe your account has been deleted,so try connecting the administrator."
+	}
+	if code == ErrNo.UserHasExisted {
+		return "This username has been registered before."
+	}
+	if code == ErrNo.PermDenied {
+		return "Sorry.You don't have permission to do so."
+	}
+	return "Success!"
+}
