a8fbc33fb5d7d6e3c5792a894a74db67d5a712f5
OceanCT <yangzhichao_hit@163.com>
2022-04-07 08:12:11 +0800

    Types

---
+++ Types/RequestAndResponseType/AddVerifyCodeRequestAndResponse/addVerifyCodeRequestAndResponse.go
@@ -0,0 +1,14 @@
+package AddVerifyCodeRequestAndResponse
+
+import "TheLabSystem/Types/RequestAndResponseType/ErrNo"
+
+type AddVerifyCodeRequest struct {
+	VerifyCode int `json:"verify_code"`
+	UserType   int `json:"user_type"`
+}
+type AddVerifyCodeResponse struct {
+	Code ErrNo.ErrNo `json:"code"`
+	Data struct {
+		Message string `json:"message"`
+	}
+}
