0b949c0d525fffef4450a957cd8e304c062661e2
OceanCT <yangzhichao_hit@163.com>
2022-04-07 08:51:10 +0800

    Controller

--- Controller/VerifyCodeController/verifyCodeController.go
+++ Controller/VerifyCodeController/verifyCodeController.go
@@ -5,6 +5,7 @@ import (
 	"TheLabSystem/Service/VerifyCodeService"
 	"TheLabSystem/Types/RequestAndResponseType/AddVerifyCodeRequestAndResponse"
 	"TheLabSystem/Types/RequestAndResponseType/ErrNo"
+	"TheLabSystem/Types/RequestAndResponseType/ViewAllVerifyCodeRequestAndResponse"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -27,6 +28,26 @@ func (controller VerifyCodeController) AddVerifyCodeController(c *gin.Context) {
 		return
 	}
 	response.Code = VerifyCodeService.VerifyCodeService{}.AddVerifyCode(request.VerifyCode, request.UserType, cookie)
+	response.Data.Message = ErrorInformation.GenerateErrorInformation(response.Code)
+	c.JSON(http.StatusOK, response)
+	return
+}
+
+func (controller VerifyCodeController) ViewAllVerifyCode(c *gin.Context) {
+	var request = &ViewAllVerifyCodeRequestAndResponse.ViewAllVerifyCodeRequest{}
+	var response = &ViewAllVerifyCodeRequestAndResponse.ViewAllVerifyCodeResponse{}
+	if err := c.ShouldBindJSON(request); err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+		return
+	}
+	cookie, err := c.Cookie("camp-session")
+	if err != nil {
+		response.Code = ErrNo.LoginRequired
+		response.Data.Message = ErrorInformation.GenerateErrorInformation(response.Code)
+		c.JSON(http.StatusOK, response)
+		return
+	}
+	response.Data.VerifyCodes, response.Code = VerifyCodeService.VerifyCodeService{}.ViewAllVerifyCode(cookie)
 	response.Data.Message = ErrorInformation.GenerateErrorInformation(response.Code)
 	c.JSON(http.StatusOK, response)
 	return
