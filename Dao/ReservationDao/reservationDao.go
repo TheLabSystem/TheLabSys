c15ada0752b9badfdb0ebd4877df7b81626c2571
JaceBelern <3493116412@qq.com>
2022-04-08 22:08:37 +0800

    Dao

--- Dao/ReservationDao/reservationDao.go
+++ Dao/ReservationDao/reservationDao.go
@@ -2,13 +2,15 @@ package ReservationDao
 
 import (
 	"TheLabSystem/Dao/DBAccessor"
+	"TheLabSystem/Types/ServiceType/Reservation"
 	"fmt"
 	"gorm.io/gorm"
 )
 
 type ReservationDao struct {
 	gorm.Model
-	applicantID uint
+	ApplicantID uint `gorm:"type:uint"`
+	Status      int  `gorm:"type:int"`
 }
 
 var db *gorm.DB
@@ -19,20 +21,32 @@ func connectDatabase() {
 	if DBErr == nil {
 		DBErr = db.AutoMigrate(&ReservationDao{})
 	} else {
-		fmt.Println("Error happened when initializing ResservationDao and creating Table in function NoticeDao.connectDatabase()")
+		fmt.Println("Error happened when initializing ReservationDao and creating Table in function NoticeDao.connectDatabase()")
 		fmt.Println(DBErr)
 	}
 }
 func init() {
 	connectDatabase()
 }
-
-func InsertReservation(applicantID uint) error {
-	var reservation ReservationDao
-	reservation.applicantID = applicantID
+func convertReservationToDao(reservation Reservation.Reservation) ReservationDao {
+	return ReservationDao{
+		ApplicantID: reservation.ApplicantID,
+		Status:      reservation.Status,
+	}
+}
+func convertDaoToReservation(reservationDao ReservationDao) Reservation.Reservation {
+	return Reservation.Reservation{
+		ReservationID: reservationDao.ID,
+		ApplicantID:   reservationDao.ApplicantID,
+		OperatingDay:  reservationDao.CreatedAt.String(),
+		Status:        reservationDao.Status,
+	}
+}
+func InsertReservation(reservation Reservation.Reservation) error {
+	var reservationDao = convertReservationToDao(reservation)
 	err := db.Transaction(func(tx *gorm.DB) error {
 		if DBErr == nil {
-			DBErr = tx.Create(&reservation).Error
+			DBErr = tx.Create(&reservationDao).Error
 		}
 		return DBErr
 	})
@@ -41,4 +55,104 @@ func InsertReservation(applicantID uint) error {
 		fmt.Println(err)
 	}
 	return err
+}
+
+func UpdateReservation(reservationID uint, status int) error {
+	var reservationDao ReservationDao
+	reservationDao.ID = reservationID
+	err := db.Transaction(
+		func(tx *gorm.DB) error {
+			if err := tx.Model(&reservationDao).Update("status", status).Error; err != nil {
+				tx.Rollback()
+				return err
+			}
+			return nil
+		})
+	if err != nil {
+		fmt.Println("更新预约状态出错")
+	}
+	return err
+}
+
+func FindReservationByID(id uint) (Reservation.Reservation, error) {
+	var reservationDao ReservationDao
+	var reservation Reservation.Reservation
+	err := db.Transaction(
+		func(tx *gorm.DB) error {
+			if err := tx.Where("id=?", id).First(&reservationDao).Error; err != nil {
+				tx.Rollback()
+				return err
+			}
+			return nil
+		})
+	if err != nil {
+		fmt.Println("查找预约出现错误")
+	} else {
+		reservation = convertDaoToReservation(reservationDao)
+	}
+	return reservation, err
+}
+
+func FindAllReservation() ([]Reservation.Reservation, error) {
+	var reservationDao []ReservationDao
+	var reservation []Reservation.Reservation
+	err := db.Transaction(
+		func(tx *gorm.DB) error {
+			if err := tx.Find(&reservationDao).Error; err != nil {
+				tx.Rollback()
+				return err
+			}
+			return nil
+		})
+	if err != nil {
+		fmt.Println("查找预约出现错误")
+	} else {
+		reservation = make([]Reservation.Reservation, len(reservationDao), len(reservationDao))
+		for key := range reservationDao {
+			reservation[key] = convertDaoToReservation(reservationDao[key])
+		}
+	}
+	return reservation, err
+}
+func FindApprovalReservation() ([]Reservation.Reservation, error) {
+	var reservationDao []ReservationDao
+	var reservation []Reservation.Reservation
+	err := db.Transaction(
+		func(tx *gorm.DB) error {
+			if err := tx.Where("status=1 or status=2 or status=3").Find(&reservationDao).Error; err != nil {
+				tx.Rollback()
+				return err
+			}
+			return nil
+		})
+	if err != nil {
+		fmt.Println("查找预约出现错误")
+	} else {
+		reservation = make([]Reservation.Reservation, len(reservationDao), len(reservationDao))
+		for key := range reservationDao {
+			reservation[key] = convertDaoToReservation(reservationDao[key])
+		}
+	}
+	return reservation, err
+}
+func FindDisapprovalReservation() ([]Reservation.Reservation, error) {
+	var reservationDao []ReservationDao
+	var reservation []Reservation.Reservation
+	err := db.Transaction(
+		func(tx *gorm.DB) error {
+			if err := tx.Where("status!=1 and status!=2 and status!=3").Find(&reservationDao).Error; err != nil {
+				tx.Rollback()
+				return err
+			}
+			return nil
+		})
+	if err != nil {
+		fmt.Println("查找预约出现错误")
+	} else {
+		reservation = make([]Reservation.Reservation, len(reservationDao), len(reservationDao))
+		for key := range reservationDao {
+			reservation[key] = convertDaoToReservation(reservationDao[key])
+		}
+	}
+	return reservation, err
 }
