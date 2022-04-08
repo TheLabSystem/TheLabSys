9153a11993f24736b8c12fff7ad68329884a4df8
OceanCT <yangzhichao_hit@163.com>
2022-04-08 23:38:59 +0800

    Controller

--- Controller/ReservationController/reservationController.go
+++ Controller/ReservationController/reservationController.go
@@ -1,1 +1,33 @@
 package ReservationController
+
+import (
+	"TheLabSystem/Config/ErrorInformation"
+	"TheLabSystem/Service/ReservationService"
+	"TheLabSystem/Types/RequestAndResponseType/ErrNo"
+	"TheLabSystem/Types/RequestAndResponseType/Reservation/SubmitReservationRequestAndResponse"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type ReservationController struct {
+}
+
+func (controller ReservationController) SubmitReservation(c *gin.Context) {
+	var request = &SubmitReservationRequestAndResponse.SubmitReservationRequest{}
+	var response = &SubmitReservationRequestAndResponse.SubmitReservationResponse{}
+	if err := c.ShouldBindJSON(request); err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+		return
+	}
+	cookie, err := c.Cookie("camp-session")
+	if err != nil {
+		response.Code = ErrNo.LoginRequired
+		response.Data.Message = ErrorInformation.GenerateErrorInformation(response.Code)
+		c.JSON(http.StatusOK, response)
+		return
+	}
+	response.Code = ReservationService.ReservationService{}.SubmitReservation(cookie, request)
+	response.Data.Message = ErrorInformation.GenerateErrorInformation(response.Code)
+	c.JSON(http.StatusOK, response)
+	return
+}
