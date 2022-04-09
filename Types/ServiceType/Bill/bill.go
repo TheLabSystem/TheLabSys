070624a321dea5084bcc160d2e6d3fae03cb22ac
JaceBelern <3493116412@qq.com>
2022-04-09 17:44:15 +0800

    Type

--- Types/ServiceType/Bill/bill.go
+++ Types/ServiceType/Bill/bill.go
@@ -1,8 +1,9 @@
 package Bill
 
 type Bill struct {
-	BillID     uint
-	PayerID    uint
-	Money      float64
-	BillStatus int
+	BillID        uint
+	ReservationID uint
+	PayerID       uint
+	Money         float64
+	BillStatus    int
 }
