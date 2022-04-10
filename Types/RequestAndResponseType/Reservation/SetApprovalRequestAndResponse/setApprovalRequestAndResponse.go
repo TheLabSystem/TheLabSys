b263d45eaf0ce9788de67e0871fed7cafc24e140
JaceBelern <3493116412@qq.com>
2022-04-10 22:54:48 +0800

    Type

--- Types/RequestAndResponseType/Reservation/SetApprovalRequestAndResponse/setApprovalRequestAndResponse.go
+++ Types/RequestAndResponseType/Reservation/SetApprovalRequestAndResponse/setApprovalRequestAndResponse.go
@@ -3,8 +3,9 @@ package SetApprovalRequestAndResponse
 import "TheLabSystem/Types/RequestAndResponseType/ErrNo"
 
 type SetApprovalRequest struct {
-	ReservationID uint `json:"reservation_id"`
-	Approval      int  `json:"approval"`
+	ReservationID uint    `json:"reservation_id"`
+	Approval      int     `json:"approval"`
+	Money         float64 `json:"money"`
 }
 type SetApprovalResponse struct {
 	Code ErrNo.ErrNo `json:"Code"`
