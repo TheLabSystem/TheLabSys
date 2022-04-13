bc516d29bc5af6f6041ef1364ecd6bc315fecea2
OceanCT <yangzhichao_hit@163.com>
2022-04-13 11:31:26 +0800

    Types

--- Types/RequestAndResponseType/Device/AddDeviceRequestAndResponse/addDeviceRequestAndResponse.go
+++ Types/RequestAndResponseType/Device/AddDeviceRequestAndResponse/addDeviceRequestAndResponse.go
@@ -5,14 +5,15 @@ import (
 )
 
 type AddDeviceRequest struct {
-	DeviceID     uint   `json:"device_id"`
-	DeviceTypeID uint   `json:"device_type_id"`
-	DeviceInfo   string `json:"device_info"`
+	DeviceID     uint    `json:"device_id"`
+	DeviceTypeID uint    `json:"device_type_id"`
+	DeviceInfo   string  `json:"device_info"`
+	Money        float64 `json:"money"`
 }
 
 type AddDeviceResponse struct {
 	Code ErrNo.ErrNo `json:"Code"`
 	Data struct {
-		Message string `json:"Message"`
+		Message string `json:"message"`
 	}
 }
