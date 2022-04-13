7249e337001b08b1c93d33e54c29636cdf0df482
OceanCT <yangzhichao_hit@163.com>
2022-04-13 12:22:30 +0800

    Types

--- Types/RequestAndResponseType/Reservation/SubmitReservationRequestAndResponse/submitReservationRequestAndResponse.go
+++ Types/RequestAndResponseType/Reservation/SubmitReservationRequestAndResponse/submitReservationRequestAndResponse.go
@@ -3,11 +3,10 @@ package SubmitReservationRequestAndResponse
 import "TheLabSystem/Types/RequestAndResponseType/ErrNo"
 
 type SubmitReservationRequest struct {
-	DeviceType  uint   `json:"deviceType"`
-	Day         string `json:"day"`
-	Time        int    `json:"time"`
-	Num         int    `json:"num"`
-	Description string `json:"description"`
+	DeviceType uint   `json:"deviceType"`
+	Day        string `json:"day"`
+	Time       int    `json:"time"`
+	Num        int    `json:"num"`
 }
 
 type SubmitReservationResponse struct {
